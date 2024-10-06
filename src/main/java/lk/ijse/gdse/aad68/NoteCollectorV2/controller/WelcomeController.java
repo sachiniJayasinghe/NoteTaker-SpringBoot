8c42dbef3d39bc8c1b581d0b1e40ed67173860ec
sachiniJayasinghe <gunasekarasachini2003@gmail.com>
2024-10-06 15:15:14 +0530

    Add thymeleaf with basic UI

---
+++ src/main/java/lk/ijse/gdse/aad68/NoteCollectorV2/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.aad68.NoteCollectorV2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api/v1/welcome")
+public class WelcomeController {
+    public String welcome(Model model){
+
+    }
+}
